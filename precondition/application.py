8190249d3ad48fa2be06d0206e7255d105463dca
Alex Tunick <alexdrakeua@gmail.com>
2017-09-20 19:57:30 +0300

    Almost full search auto test suit

--- precondition/application.py
+++ precondition/application.py
@@ -238,7 +238,172 @@ class Application:
      self.driver.get_screenshot_as_file("middle_high_empty_tfield.png")
 
  # ==============================================================================================================
- # ======================================== TEST SEARCH BY *STATE* CRITERION ===================================
+ # ======================================== TEST SEARCH WITH ONE LETTER(Input) AND STATE, ZIPCODE ===============
  # ==============================================================================================================
 
- def search_school_state
+ def search_d_letter_allstates(self):
+     self.driver.find_element_by_xpath(".//*[@id='root']/div/div[2]/div/form/div[1]/div[2]/input").send_keys("d")
+     self.driver.find_element_by_xpath("//button[@type='submit']").click()
+     time.sleep(3)
+     #ASSERT
+     self.driver.find_element_by_xpath(".//*[@id='root']/div/div[2]/div/form/div[1]/div/div[1]/div/label[1]/div")
+     self.driver.find_element_by_xpath(".//*[@id='root']/div/div[2]/div/form/div[1]/div/div[1]/div/label[2]/div")
+     self.driver.find_element_by_xpath(".//*[@id='root']/div/div[2]/div/form/div[1]/div/div[1]/div/label[3]/div")
+     self.driver.find_element_by_xpath(".//*[@id='root']/div/div[2]/div/form/div[1]/div/div[1]/div/label[4]/div")
+     self.driver.find_element_by_xpath(".//*[@id='root']/div/div[2]/div/form/div[1]/div/div[1]/div/label[5]/div")
+     self.driver.get_screenshot_as_file("d_letter_allstates.png")
+
+ def search_daycare_s_letter_state(self):
+     self.driver.find_element_by_css_selector(".homePage__closed-arrow___2HRFM").click()
+     self.driver.find_element_by_xpath(".//*[@id='react-select-2--option-1']").click()
+     self.driver.find_element_by_xpath(".//*[@id='root']/div/div[2]/div/form/div[1]/div[2]/input").send_keys("s")
+     # check box
+     self.driver.find_element_by_xpath(".//*[@id='root']/div/div[2]/div/form/div[2]/div[1]/div/label").click()
+     self.driver.find_element_by_xpath("//button[@type='submit']").click()
+     time.sleep(3)
+     # ASSERT
+     self.driver.find_element_by_xpath(".//*[@id='root']/div/div[2]/div/form/div[1]/div/div[1]/div/label/div")
+     self.driver.get_screenshot_as_file("s_letter__daycare_state.png")
+
+ def search_elementary_a_letter_state(self):
+     self.driver.find_element_by_css_selector(".homePage__closed-arrow___2HRFM").click()
+     self.driver.find_element_by_xpath(".//*[@id='react-select-2--option-37']").click()
+     self.driver.find_element_by_xpath(".//*[@id='root']/div/div[2]/div/form/div[1]/div[2]/input").send_keys("a")
+     # check box
+     self.driver.find_element_by_xpath(".//*[@id='root']/div/div[2]/div/form/div[2]/div[2]/div/label").click()
+     time.sleep(1)
+     self.driver.find_element_by_xpath("//button[@type='submit']").click()
+     time.sleep(3)
+     # ASSERT
+     self.driver.find_element_by_xpath(".//*[@id='root']/div/div[2]/div/form/div[1]/div/div[1]/div/label[1]/div")
+     self.driver.find_element_by_xpath(".//*[@id='root']/div/div[2]/div/form/div[1]/div/div[1]/div/label[2]/div")
+     self.driver.get_screenshot_as_file("a_letter__elementary_state.png")
+
+ def search_middle_b_letter_state(self):
+     self.driver.find_element_by_css_selector(".homePage__closed-arrow___2HRFM").click()
+     self.driver.find_element_by_xpath(".//*[@id='react-select-2--option-7']").click()
+     self.driver.find_element_by_xpath(".//*[@id='root']/div/div[2]/div/form/div[1]/div[2]/input").send_keys("j")
+     # check box
+     self.driver.find_element_by_xpath(".//*[@id='root']/div/div[2]/div/form/div[2]/div[3]/div/label").click()
+     time.sleep(1)
+     self.driver.find_element_by_xpath("//button[@type='submit']").click()
+     time.sleep(3)
+     # ASSERT
+     self.driver.find_element_by_xpath(".//*[@id='root']/div/div[2]/div/form/div[1]/div/div[1]/div/label[1]/div")
+     self.driver.find_element_by_xpath(".//*[@id='root']/div/div[2]/div/form/div[1]/div/div[1]/div/label[2]/div")
+     self.driver.find_element_by_xpath(".//*[@id='root']/div/div[2]/div/form/div[1]/div/div[1]/div/label[3]/div")
+     self.driver.get_screenshot_as_file("j_letter__middle_state.png")
+
+ def search_high_s_letter_state(self):
+     self.driver.find_element_by_css_selector(".homePage__closed-arrow___2HRFM").click()
+     self.driver.find_element_by_xpath(".//*[@id='react-select-2--option-7']").click()
+     self.driver.find_element_by_xpath(".//*[@id='root']/div/div[2]/div/form/div[1]/div[2]/input").send_keys("s")
+     # check box
+     self.driver.find_element_by_xpath(".//*[@id='root']/div/div[2]/div/form/div[2]/div[4]/div/label").click()
+     time.sleep(1)
+     self.driver.find_element_by_xpath("//button[@type='submit']").click()
+     time.sleep(3)
+     # ASSERT
+     self.driver.find_element_by_xpath(".//*[@id='root']/div/div[2]/div/form/div[1]/div/div[1]/div/label[1]/div")
+     self.driver.get_screenshot_as_file("s_letter__high_state.png")
+
+ def search_college_s_letter_state(self):
+     self.driver.find_element_by_css_selector(".homePage__closed-arrow___2HRFM").click()
+     self.driver.find_element_by_xpath(".//*[@id='react-select-2--option-6']").click()
+     self.driver.find_element_by_xpath(".//*[@id='root']/div/div[2]/div/form/div[1]/div[2]/input").send_keys("s")
+     # check box
+     self.driver.find_element_by_xpath(".//*[@id='root']/div/div[2]/div/form/div[2]/div[5]/div/label").click()
+     time.sleep(1)
+     self.driver.find_element_by_xpath("//button[@type='submit']").click()
+     time.sleep(3)
+     self.driver.find_element_by_xpath(".//*[@id='root']/div/div[2]/div/form/div[1]/div/div[1]/div/label[1]/div")
+     self.driver.get_screenshot_as_file("s_letter__college_state.png")
+
+ def search_elem_middle_s_letter_state(self):
+     self.driver.find_element_by_css_selector(".homePage__closed-arrow___2HRFM").click()
+     self.driver.find_element_by_xpath(".//*[@id='react-select-2--option-6']").click()
+     self.driver.find_element_by_xpath(".//*[@id='root']/div/div[2]/div/form/div[1]/div[2]/input").send_keys("s")
+     # check box
+     self.driver.find_element_by_xpath(".//*[@id='root']/div/div[2]/div/form/div[2]/div[2]/div/label").click()
+     self.driver.find_element_by_xpath(".//*[@id='root']/div/div[2]/div/form/div[2]/div[3]/div/label").click()
+     time.sleep(1)
+     self.driver.find_element_by_xpath("//button[@type='submit']").click()
+     time.sleep(3)
+     # ASSERT
+     self.driver.find_element_by_xpath(".//*[@id='root']/div/div[2]/div/form/div[1]/div/div[1]/div/label[1]/div")
+     self.driver.find_element_by_xpath(".//*[@id='root']/div/div[2]/div/form/div[1]/div/div[1]/div/label[2]/div")
+     self.driver.find_element_by_xpath(".//*[@id='root']/div/div[2]/div/form/div[1]/div/div[1]/div/label[3]/div")
+     self.driver.get_screenshot_as_file("s_letter__elem_middle_state.png")
+
+ def search_elem_high_s_letter_d_letter_state(self):
+     self.driver.find_element_by_css_selector(".homePage__closed-arrow___2HRFM").click()
+     self.driver.find_element_by_xpath(".//*[@id='react-select-2--option-6']").click()
+     self.driver.find_element_by_xpath(".//*[@id='root']/div/div[2]/div/form/div[1]/div[2]/input").send_keys("d")
+     # check box
+     self.driver.find_element_by_xpath(".//*[@id='root']/div/div[2]/div/form/div[2]/div[2]/div/label").click()
+     self.driver.find_element_by_xpath(".//*[@id='root']/div/div[2]/div/form/div[2]/div[4]/div/label").click()
+     time.sleep(1)
+     self.driver.find_element_by_xpath("//button[@type='submit']").click()
+     time.sleep(3)
+     # ASSERT
+     self.driver.find_element_by_xpath(".//*[@id='root']/div/div[2]/div/form/div[1]/div/div[1]/div/label[1]/div")
+     self.driver.find_element_by_xpath(".//*[@id='root']/div/div[2]/div/form/div[1]/div/div[1]/div/label[2]/div")
+     self.driver.find_element_by_xpath(".//*[@id='root']/div/div[2]/div/form/div[1]/div/div[1]/div/label[3]/div")
+     self.driver.get_screenshot_as_file("d_letter__elem_high_state.png")
+
+ def search_midl_high_s_letter_state(self):
+     self.driver.find_element_by_css_selector(".homePage__closed-arrow___2HRFM").click()
+     self.driver.find_element_by_xpath(".//*[@id='react-select-2--option-6']").click()
+     self.driver.find_element_by_xpath(".//*[@id='root']/div/div[2]/div/form/div[1]/div[2]/input").send_keys("s")
+     # check box
+     self.driver.find_element_by_xpath(".//*[@id='root']/div/div[2]/div/form/div[2]/div[3]/div/label").click()
+     self.driver.find_element_by_xpath(".//*[@id='root']/div/div[2]/div/form/div[2]/div[4]/div/label").click()
+     time.sleep(1)
+     self.driver.find_element_by_xpath("//button[@type='submit']").click()
+     time.sleep(3)
+     # ASSERT
+     self.driver.find_element_by_xpath(".//*[@id='root']/div/div[2]/div/form/div[1]/div/div[1]/div/label[1]/div")
+     self.driver.find_element_by_xpath(".//*[@id='root']/div/div[2]/div/form/div[1]/div/div[1]/div/label[2]/div")
+     self.driver.find_element_by_xpath(".//*[@id='root']/div/div[2]/div/form/div[1]/div/div[1]/div/label[3]/div")
+     self.driver.get_screenshot_as_file("s_letter__middle_high_state.png")
+
+ def search_zip_code_with_state(self):
+     self.driver.find_element_by_css_selector(".homePage__closed-arrow___2HRFM").click()
+     self.driver.find_element_by_xpath(".//*[@id='react-select-2--option-6']").click()
+     self.driver.find_element_by_xpath(".//*[@id='root']/div/div[2]/div/form/div[1]/div[2]/input").send_keys("90005")
+     time.sleep(1)
+     self.driver.find_element_by_xpath("//button[@type='submit']").click()
+     time.sleep(3)
+     self.driver.find_element_by_xpath("//button[@type='submit']").click()
+     # ASSERT
+     self.driver.find_element_by_xpath(".//*[@id='root']/div/div[2]/div/form/div[1]/div/div[1]/div/label[1]/div")
+     self.driver.find_element_by_xpath(".//*[@id='root']/div/div[2]/div/form/div[1]/div/div[1]/div/label[2]/div")
+     self.driver.find_element_by_xpath(".//*[@id='root']/div/div[2]/div/form/div[1]/div/div[1]/div/label[3]/div")
+     self.driver.find_element_by_xpath(".//*[@id='root']/div/div[2]/div/form/div[1]/div/div[1]/div/label[4]/div")
+     self.driver.get_screenshot_as_file("zipcode_with_state.png")
+
+ def search_zip_code_no_criteria(self):
+     self.driver.find_element_by_xpath(".//*[@id='root']/div/div[2]/div/form/div[1]/div[2]/input").send_keys("90005")
+     time.sleep(1)
+     # ASSERT
+     self.driver.find_element_by_xpath("//button[@type='submit']").click()
+     self.driver.find_element_by_xpath(".//*[@id='root']/div/div[2]/div/form/div[1]/div/div[1]/div/label[1]/div")
+     self.driver.find_element_by_xpath(".//*[@id='root']/div/div[2]/div/form/div[1]/div/div[1]/div/label[2]/div")
+     self.driver.find_element_by_xpath(".//*[@id='root']/div/div[2]/div/form/div[1]/div/div[1]/div/label[3]/div")
+     self.driver.find_element_by_xpath(".//*[@id='root']/div/div[2]/div/form/div[1]/div/div[1]/div/label[4]/div")
+     self.driver.get_screenshot_as_file("zipcode_no_criteria.png")
+
+ def search_city_no_state(self):
+     self.driver.find_element_by_xpath(".//*[@id='root']/div/div[2]/div/form/div[1]/div[2]/input").send_keys("Los Angeles")
+     time.sleep(1)
+     self.driver.find_element_by_xpath(".//*[@id='root']/div/div[2]/div/form/div[1]/div[2]/div/div[1]/span").click()
+     time.sleep(1)
+     self.driver.find_element_by_xpath("//button[@type='submit']").click()
+     time.sleep(3)
+     self.driver.find_element_by_xpath("//button[@type='submit']").click()
+     self.driver.find_element_by_xpath(".//*[@id='root']/div/div[2]/div/form/div[1]/div/div[1]/div/label[1]/div")
+     self.driver.find_element_by_xpath(".//*[@id='root']/div/div[2]/div/form/div[1]/div/div[1]/div/label[2]/div")
+     self.driver.find_element_by_xpath(".//*[@id='root']/div/div[2]/div/form/div[1]/div/div[1]/div/label[3]/div")
+     self.driver.find_element_by_xpath(".//*[@id='root']/div/div[2]/div/form/div[1]/div/div[1]/div/label[4]/div")
+     self.driver.find_element_by_xpath(".//*[@id='root']/div/div[2]/div/form/div[1]/div/div[1]/div/label[5]/div")
+     self.driver.get_screenshot_as_file("city_no_state.png")
--- test_search/test_search.py
+++ test_search/test_search.py
@@ -25,9 +25,9 @@ def test_search_colleges(app):
     app.login()
     app.search_colleges()
     app.logout()
-#=================== with EMPTY TEXT FIELD ===========================
-#=================== with EMPTY TEXT FIELD ===========================
 
+#=================== with EMPTY TEXT FIELD ===========================
+#=================== with EMPTY TEXT FIELD ===========================
 def test_search_daycare_empty(app):
     app.login()
     app.search_daycare_empty_field()
@@ -68,6 +68,64 @@ def test_search_middle_high_empty(app):
     app.search_middle_high_empty_field()
     app.logout()
 
+#=================== SEARCH WITH ONE LETTER AND STATE ===========================
+#=================== SEARCH WITH ONE LETTER AND STATE ===========================
+def test_search_d_letter_all_states(app):
+    app.login()
+    app.search_d_letter_allstates()
+    app.logout()
 
+def test_search_s_letter_daycare_state(app):
+    app.login()
+    app.search_daycare_s_letter_state()
+    app.logout()
 
+def test_search_a_letter_elem_state(app):
+    app.login()
+    app.search_elementary_a_letter_state()
+    app.logout()
 
+def test_search_j_letter_middle_state(app):
+    app.login()
+    app.search_middle_b_letter_state()
+    app.logout()
+
+def test_search_s_letter_high_state(app):
+    app.login()
+    app.search_high_s_letter_state()
+    app.logout()
+
+def test_search_s_letter_college_state(app):
+    app.login()
+    app.search_college_s_letter_state()
+    app.logout()
+
+def test_search_s_letter_elem_middle_state(app):
+    app.login()
+    app.search_elem_middle_s_letter_state()
+    app.logout()
+
+def test_search_d_letter_elem_high_state(app):
+    app.login()
+    app.search_elem_high_s_letter_d_letter_state()
+    app.logout()
+
+def test_search_s_letter_mid_high_state(app):
+    app.login()
+    app.search_midl_high_s_letter_state()
+    app.logout()
+
+def test_search_zipcode_with_state(app):
+    app.login()
+    app.search_zip_code_with_state()
+    app.logout()
+
+def test_zipcode_no_criterion(app):
+    app.login()
+    app.search_zip_code_no_criteria()
+    app.logout()
+
+def test_search_city_no_state(app):
+    app.login()
+    app.search_city_no_state()
+    app.logout()
